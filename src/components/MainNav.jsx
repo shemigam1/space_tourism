97957f088f6062e2e0984eff721db2ecf00df457
semilore <semilore@omotade-michaels.com>
2024-02-02 04:30:23 +0100

    fix: nav

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -11,15 +11,15 @@ const MainNav = () => {
             <div className="pl-10">
                 <img src="../../src/assets/shared/logo.svg" alt="" />
             </div>
-            <nav className="flex w-1/2 h-full justify-evenly text-white items-center pr-16 backdrop-blur-2xl border border-transparent hidden md:block gap-4">
+            <nav className="md:flex w-1/2 h-full justify-evenly text-white items-center pr-16 backdrop-blur-2xl border border-transparent hidden md:gap-4">
                 <NavLink to="/" className="h-full flex-row justify-center items-center gap-3 focus:border-b-white"><span>00</span> Home</NavLink>
                 <NavLink to="/destination" className="h-full flex-row justify-center items-center gap-3"><span>01</span> Destination</NavLink>
                 <NavLink to="/crew" className="h-full flex-row justify-center items-center gap-3"><span>02</span> Crew</NavLink>
                 <NavLink to="/technology" className="h-full flex-row justify-center items-center gap-3"><span>03</span> Technology</NavLink>
             </nav>
-            {/* <div onClick={handleHamb} className="pr-10 hover:cursor-pointer md:hidden">
+            <div onClick={handleHamb} className="pr-10 hover:cursor-pointer md:hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21"><g fill="#D0D6F9" fill-rule="evenodd"><path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" /></g></svg>
-            </div> */}
+            </div>
         </div>
     )
 }
